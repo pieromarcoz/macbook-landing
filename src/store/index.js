9cc5aedc226c786b11922e830d1e430d23c9d396
pieromarcoz <75951868+pieromarcoz@users.noreply.github.com>
2025-12-07 04:31:03 -0500

    fix

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,15 @@
 import {create} from 'zustand';
 
 export const  useMacbookStore = create((set) => ({
-    color: "#2e2c2e",
+    color: '#2e2c2e',
     setColor: (color) => set({ color }),
+
     scale: 0.08,
-    setScale: (scale) => set({scale}),
-    reset: () => set({color: "#2e2c2e", scale: 0.08}),
+    setScale: (scale) => set({ scale }),
+
+    texture: '/videos/feature-1.mp4',
+    setTexture: (texture) => set({ texture }),
+
+    reset: () => set({ color: '#2e2c2e', scale: 0.08, texture: '/videos/feature-1.mp4' }),
 }));
 
